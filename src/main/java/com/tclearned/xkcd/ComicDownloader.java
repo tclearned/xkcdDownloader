2aff1899f6e048c5742ba8aa6a236ddd2aedf45f
Michael Sullivan <sparklytarnish+git@protonmail.com>
2020-06-09 20:57:17 -0400

    Fixed bug where image wouldn't actually get downloaded

--- src/main/java/com/tclearned/xkcd/ComicDownloader.java
+++ src/main/java/com/tclearned/xkcd/ComicDownloader.java
@@ -26,24 +26,27 @@ public class ComicDownloader {
         ObjectMapper mapper = new ObjectMapper();
         // output is what is going to actually be storing the image that we will then write to a file
         ByteArrayOutputStream output = new ByteArrayOutputStream();
-        try(InputStream is = downloadURL.openStream()) {
+        URL imageURL;
+        try {
             Comic current = mapper.readValue(downloadURL, Comic.class);
-            URL imageURL = new URL(current.image);
+            imageURL = new URL(current.image);
+        }  catch (IOException e) {
+            System.err.println("Failed to download from url: " + downloadURL);
+            e.printStackTrace();
+            return new byte[0];
+        }
 
+        try(InputStream is = imageURL.openStream()) {
             // downloading image to byte array
             // image is downloaded in 1 Kilobyte sized chunks and then appended to the output stream
-            int n = 0;
+            int n;
             byte[] buffer = new byte[1024];
 
             while((n = is.read(buffer)) != -1) {
                 output.write(buffer, 0, n);
             }
-        } catch (MalformedURLException e) {
-            System.err.println("Image url is malformed for URL: " + downloadURL);
-            System.err.println("This should never happen");
-            e.printStackTrace();
         } catch (IOException e) {
-            System.err.println("Failed to download from url: " + downloadURL);
+            System.err.println("Failed to download image from url: " + imageURL);
             e.printStackTrace();
         }
         return output.toByteArray();
