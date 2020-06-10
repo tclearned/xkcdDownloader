1dac42fd34fec32bf66cd2191ce9c8b91f8cdf8d
Michael Sullivan <sparklytarnish+git@protonmail.com>
2020-06-09 21:35:40 -0400

    Removed unnecessary import

--- src/main/java/com/tclearned/xkcd/Comic.java
+++ src/main/java/com/tclearned/xkcd/Comic.java
@@ -1,7 +1,6 @@
 package com.tclearned.xkcd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
